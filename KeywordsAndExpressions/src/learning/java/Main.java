85b0c1389d1174958e1d0ed5504676f2331e14ab
Collin Clifford <83039749+CollinClifford@users.noreply.github.com>
2022-02-25 23:02:39 -0800

    Day four.

---
+++ KeywordsAndExpressions/src/learning/java/Main.java
@@ -0,0 +1,14 @@
+package learning.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //identify the expressions
+	    int score = 100; // score = 100
+        if (score > 99) { // score > 99
+            System.out.println("You got the high score!"); // "You got the high score!"
+            score = 0; // score = 0
+        }
+    }
+}
---
+++ StatementsWhiteSpaceAndIndentations/src/learning/java/Main.java
@@ -0,0 +1,12 @@
+package learning.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myVariable=50; // entire line is statement.
+        myVariable++;
+        myVariable--; //also complete statements.
+        System.out.println("This is a test"); // statement
+
+    }
+}
