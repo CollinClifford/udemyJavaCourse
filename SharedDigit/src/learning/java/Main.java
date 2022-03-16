81be2aa5ea693ec1552a9a43436c5556a72b9adc
Collin Clifford <83039749+CollinClifford@users.noreply.github.com>
2022-03-15 22:42:19 -0700

    anotherproblem

---
+++ SharedDigit/src/learning/java/Main.java
@@ -0,0 +1,45 @@
+package learning.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(hasSharedDigit(12, 23)); // true
+        System.out.println(hasSharedDigit(9, 99)); // false
+        System.out.println(hasSharedDigit(15, 55)); // true
+    }
+    // Write a method named hasSharedDigit with two parameters of type int.
+    // Each number should be within the range of 10 (inclusive) - 99 (inclusive). If one of the numbers is not within
+    // the range, the method should return false.
+    // The method should return true if there is a digit that appears in both numbers, such as 2 in 12 and 23;
+    // otherwise, the method should return false.
+    //
+    public static boolean hasSharedDigit(int digitOne, int digitTwo) {
+        int lastDigitOne;
+        int firstDigitOne;
+        int lastDigitTwo;
+        int firstDigitTwo;
+        if ((digitOne < 10 || digitOne > 99) || (digitTwo < 10 || digitTwo > 99)) {
+            return false;
+        }
+        lastDigitOne = digitOne % 10;
+        firstDigitOne = digitOne / 10;
+        lastDigitTwo = digitTwo % 10;
+        firstDigitTwo = digitTwo / 10;
+        if (lastDigitOne == lastDigitTwo
+                || lastDigitOne == firstDigitTwo
+                || firstDigitOne == lastDigitTwo
+                || firstDigitOne == firstDigitTwo) {
+            return true;
+        }
+        return false;
+    }
+    // EXAMPLE INPUT/OUTPUT:
+    // * hasSharedDigit(12, 23); → should return true since the digit 2 appears in both numbers
+    // * hasSharedDigit(9, 99); → should return false since 9 is not within the range of 10-99
+    // * hasSharedDigit(15, 55); → should return true since the digit 5 appears in both numbers
+    //
+    // NOTE: The method hasSharedDigit should be defined as public static like we have been doing so far in the
+    // course.
+    // NOTE: Do not add a main method to the solution code.
+}
