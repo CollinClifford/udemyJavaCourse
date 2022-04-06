4bb3b42f8ad328d84b63486367e52b539dc5b319
Collin Clifford <83039749+CollinClifford@users.noreply.github.com>
2022-04-05 19:49:09 -0700

    Add files via upload

---
+++ ArrayChallenge/src/learning/java/Main.java
@@ -0,0 +1,67 @@
+package learning.java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+        int[] sortedArray = sortIntegers(myIntegers);
+        printArray(sortedArray);
+    }
+
+    public static int[] getIntegers(int number) {
+        int[] values = new int[number];
+        for (int i = 0; i < values.length; i++) {
+            System.out.println(values[i]);
+            values[i] = scanner.nextInt();
+            System.out.println(values[i]);
+        }
+        System.out.println(values);
+        return values;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+//        int[] sortedArray = new int[array.length];
+//        for (int i = 0; i < array.length; i++) {
+//            sortedArray[i] = array[i];
+//        }
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+        boolean flag = true;
+        int temp;
+
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+    }
+
+    // Create a program using arrays that sorts a list of integers in descending order.
+    // Descending order is highest value to lowest.
+    // In other words if the array had the values in it 106, 26, 81, 5, 15 your program should
+    // ultimately have an array with 106,81,26, 15, 5 in it.
+    // Set up the program so that the numbers to sort are read in from the keyboard.
+    // Implement the following methods - getIntegers, printArray, and sortIntegers
+    // getIntegers returns an array of entered integers from keyboard
+    // printArray prints out the contents of the array
+    // and sortIntegers should sort the array and return a new array containing the sorted numbers
+    // you will have to figure out how to copy the array elements from the passed array into a new
+    // array and sort them and return the new sorted array.
+}
